dc9483d18195fc5fcbb11e0c7818a18534e49205
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2023-03-01 08:37:09 -0500

    Make form mobile friendly

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import styles from './TodoForm.module.css';
-import { useAppDispatch } from '../app/hooks';
+import React from "react"
+import styles from "./TodoForm.module.css"
+import { useAppDispatch } from "../app/hooks"
 
 function TodoForm() {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
   function addTask(event: React.FormEvent<HTMLFormElement>) {
-    const input = event.currentTarget[0] as HTMLInputElement;
-    event.preventDefault();
-    dispatch({ type: 'todo/addTodo', payload: input.value })
-    input.value = '';
+    const input = event.currentTarget[0] as HTMLInputElement
+    event.preventDefault()
+    dispatch({ type: "todo/addTodo", payload: input.value })
+    input.value = ""
   }
   return (
-    <form onSubmit={addTask}>
-      <label>What are we going to do today?</label>
-      <div className={styles["todo-input-button"]}>
-        <input
-          type="text"
-          name="new-todo"
-          placeholder="Conquer the world!"
-        />
-        <button className={styles['add-task']} type="submit">Add Todo</button>
-      </div>
-    </form>
-  );
+    <>
+      <div className={styles["form-background-blur"]}></div>
+      <form onSubmit={addTask}>
+        <label>What are we going to do today?</label>
+        <div className={styles["todo-input-button"]}>
+          <input type="text" name="new-todo" placeholder="Conquer the world!" />
+          <button className={styles["add-task"]} type="submit">
+            Add Todo
+          </button>
+        </div>
+      </form>
+    </>
+  )
 }
 
-export default TodoForm;
+export default TodoForm
